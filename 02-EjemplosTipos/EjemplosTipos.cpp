b9c274d3f71f0313292a1d4d3aa4efb0929f135f
ED <elider9221@gmail.com>
2018-04-16 13:21:40 -0300

    Actualizacion

---
+++ 02-EjemplosTipos/EjemplosTipos.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Usando tipo de dato entero
+    int a = 3;
+    int b = 5;
+    cout << a+b <<endl;
+
+    // Usando tipo de dato double
+
+    double c = 2.3;
+    double d = 4.1;
+    cout << c+d << endl;
+
+    // Usando tipo de dato unsigned
+
+    unsigned e = 5;
+    unsigned f = 4;
+    cout << e-f << endl;
+
+    // Usando tipo de dato char
+
+    char g = 'B';
+    char h = 'b';
+    cout << "Letra Mayuscula " << g << " Letra Minuscula " << h << endl;
+
+    // Usando tipo de dato string
+
+    string i = "Hola como estas? ";
+    string j = " Bien";
+    cout << i << j << endl;
+
+    // Usando tipo de dato bool
+
+    bool variable = true;
+    cout << variable;
+
+}
+
