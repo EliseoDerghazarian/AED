2809ca76f1555040fcc2d38227e6d51e75841ead
ED <elider9221@gmail.com>
2018-08-19 16:11:52 -0300

    Mejora
    Mejora con lo aprendido en clase

--- 02-EjemplosTipos/EjemplosTipos.cpp
+++ 02-EjemplosTipos/EjemplosTipos.cpp
@@ -5,46 +5,57 @@ ED
 */
 
 #include <iostream>
+#include <cassert>
+#include <string>
 
-using namespace std;
+using std::string;
 
-int main()
-{
+int main(){
+
     // Usando tipo de dato entero
-    int a = 3;
-    int b = 5;
-    cout << a+b <<endl;
+    int a{3}, b{5};
+    assert(8==(a+b));
+    assert(15==(a*b));
+    assert(2==(b-a));
 
     // Usando tipo de dato double
 
-    double c = 2.3;
-    double d = 4.1;
-    cout << c+d << endl;
+    double c{4.5}, d{2.4};
+    assert(2.1==(c-d));
+    assert(80.7!=(c+d));
+    assert(c!=d);
 
     // Usando tipo de dato unsigned
 
-    unsigned e = 5;
-    unsigned f = 4;
-    cout << e-f << endl;
+    unsigned e{5}, f{5};
+    assert(e==f);
+    assert(1==(e/f));
+    assert(0==(e-f));
 
     // Usando tipo de dato char
 
-    char g = 'B';
-    char h = 'b';
-    cout << "Letra Mayuscula " << g << " Letra Minuscula " << h << endl;
+    char g{'B'}, h{'b'};
+    assert(66==g);
+    assert(98==h);
+    assert(164==(g+h));
 
     // Usando tipo de dato string
 
-    string i = "Hola como estas? ";
-    string j = " Bien";
-    cout << i << j << endl;
+    string i="Soleado";
+    string j="Nublado";
+    assert(7==i.length());
+    assert(i.length()>=j.length());
+    assert(10!=j.length());
 
     // Usando tipo de dato bool
 
-    bool k = true;
-    cout << k;
-    bool l = false;
-    cout << l;
+    bool k{true};
+    k=k and true;
+    assert(true==k);
+
+    bool l{false};
+    l=l or false;
+    assert(false==l);
 
 }
 
